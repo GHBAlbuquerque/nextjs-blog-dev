cc58474d0b81e0a5541b3086efaca936dbbf02bc
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-07-08 22:38:04 -0300

    feature: adds subtitle to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,24 +4,31 @@ import Link from "next/link";
 export default function Header() {
   return (
     <>
-      <header>
+      <header className={clsx("py-8", "sm:py-10", "md:py-11", "lg:py-12")}>
         <h1
           className={clsx(
-            "text-4xl/normal",
+            "text-4xl/tight",
             "font-extrabold",
-            "py-8",
-            "sm:text-5xl/normal",
-            "sm:py-10",
-            "md:text-6xl/normal",
-            "sm:py-11",
-            "lg:text-7xl/normal",
-            "lg:py-12"
+            "sm:text-5xl/tight",
+            "md:text-6xl/tight",
+            "lg:text-7xl/tight"
           )}
         >
           <Link href="" className="href">
             The Planet Blog
           </Link>
         </h1>
+        <h2
+          className={clsx(
+            "text-md/tight",
+            "font-thin",
+            "sm:text-lg/tight",
+            "md:text-xl/tight",
+            "lg:text-2xl/tight"
+          )}
+        >
+          Discover new worlds and galaxies
+        </h2>
       </header>
     </>
   );
