598e823eed1edb1c3eb450fb9f6afdd11656dcab
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-07-20 18:43:53 -0300

    fix: removes confirm window

--- src/components/DeletePostButton/index.tsx
+++ src/components/DeletePostButton/index.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { deletePostAction } from "@/actions/posts/delete-post-action";
 import clsx from "clsx";
@@ -6,42 +6,39 @@ import { Trash2Icon } from "lucide-react";
 import { useTransition } from "react";
 
 type DeletePostButtonProps = {
-    id: string;
-    title: string;
-}
+  id: string;
+  title: string;
+};
 
-export default function DeletePostButton({id, title} : DeletePostButtonProps) {
-   const [isPending, startTransition] = useTransition();
-    
-    async function handleClick(){
+export default function DeletePostButton({ id, title }: DeletePostButtonProps) {
+  const [isPending, startTransition] = useTransition();
+
+  function handleClick() {
         startTransition(async () => {
-            console.log(isPending)
-            const result = await deletePostAction(id);
-            alert("Result é " + result)
-        })
-  
-    }
+        const result = await deletePostAction(id);
+        alert("Post deleted! " + result);
+        });
+  }
 
-    return (
-        <button
-                className={clsx(
-                  "text-red-400",
-                  "cursor-pointer",
-                  "[&_svg]:w-4",
-                  "[&_svg]:h-4",
-                  "hover:scale-130",
-                  "hover:text-red-600",
-                  "transition",
-                  "disabled:text-slate-600",
-                  "disabled:cursor-not-allowed"
-                )}
-                aria-label={`Delete post: ${title}`}
-                title={`Delete post: ${title}`}
-                onClick={handleClick}
-                disabled={isPending}
-              >
-                <Trash2Icon />
-              </button>
-    )
-
+  return (
+    <button
+      className={clsx(
+        "text-red-400",
+        "cursor-pointer",
+        "[&_svg]:w-4",
+        "[&_svg]:h-4",
+        "hover:scale-130",
+        "hover:text-red-600",
+        "transition",
+        "disabled:text-slate-600",
+        "disabled:cursor-not-allowed"
+      )}
+      aria-label={`Delete post: ${title}`}
+      title={`Delete post: ${title}`}
+      onClick={handleClick}
+      disabled={isPending}
+    >
+      <Trash2Icon />
+    </button>
+  );
 }
