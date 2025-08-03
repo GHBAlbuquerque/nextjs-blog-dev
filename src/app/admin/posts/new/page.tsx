f40e4c2d689c3da17c0d08d7643713749aa6df15
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-08-03 18:32:55 -0300

    feat: reates input text

--- src/app/admin/posts/new/page.tsx
+++ src/app/admin/posts/new/page.tsx
@@ -1,12 +1,25 @@
 import Button from "@/components/Button";
+import { InputText } from "@/components/InputText";
 import { BanIcon, BugIcon, CheckIcon } from "lucide-react";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
-export default async function AdminPostsNewPage(){
-    return <div className="py-16 flex gap-4 flex-wrap items-center">
-    <Button variant="default" size='lg'><CheckIcon/> Confirm</Button>
-    <Button variant="ghost" size='md'><BanIcon/> Cancel</Button>
-    <Button variant="danger" size='sm'><BugIcon/> Delete</Button>
-    </div>
+export default async function AdminPostsNewPage() {
+  return (
+    <>
+      <InputText labelText="Post title" placeholder="Type here" disabled/>
+      
+      <div className="py-16 flex gap-4 flex-wrap items-center">
+        <Button variant="default" size="lg">
+          <CheckIcon /> Confirm
+        </Button>
+        <Button variant="ghost" size="md">
+          <BanIcon /> Cancel
+        </Button>
+        <Button variant="danger" size="sm">
+          <BugIcon /> Delete
+        </Button>
+      </div>
+    </>
+  );
 }
---
+++ src/components/InputText/index.tsx
@@ -0,0 +1,39 @@
+import clsx from "clsx";
+import { useId } from "react";
+
+type InputTextProps = {
+  labelText?: string;
+} & React.ComponentProps<"input">;
+
+export function InputText({ labelText = "", ...props }: InputTextProps) {
+  const id = useId();
+
+  return (
+    <div className="flex flex-col gap-2">
+      {labelText && (
+        <label className="text-sm" htmlFor={id}>
+          {labelText}
+        </label>
+      )}
+      <input
+        className={clsx(
+          "bg-white",
+          "outline-0",
+          "ring-2",
+          "ring-slate-400",
+          "rounded",
+          "py-2",
+          "px-2",
+          "transition",
+          "focus:ring-blue-600",
+          "placeholder-slate-300",
+          "disabled:bg-slate-200",
+          "disabled:cursor-not-allowed",
+          props.className
+        )}
+        {...props}
+        id={id}
+      />
+    </div>
+  );
+}
