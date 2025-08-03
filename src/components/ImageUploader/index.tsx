69831f7fcadfaa98714efb57b7220cd309680de5
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-08-03 19:49:43 -0300

    feat: creates upload img input - wip

--- src/components/ImageUploader/index.tsx
+++ src/components/ImageUploader/index.tsx
@@ -3,20 +3,46 @@
 import { ImageUpIcon } from "lucide-react";
 import Button from "../Button";
 import { useRef } from "react";
+import { IMAGE_UPLOAD_MAX_SIZE } from "@/lib/post/constants";
+import { toast } from "react-toastify";
 
 export default function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null);
 
-  function handleImageUpload() {
+  function handleChooseImage() {
     if(!fileInputRef.current) return;
-
     fileInputRef.current.click();
   }
+
+  function handleChange(){
+    if(!fileInputRef.current) return;
+    const fileInput = fileInputRef.current;
+    const file = fileInput.files?.[0]; // it is possible yo upload multiple files, but in this case we have only one
+
+    if(!file) return;
+
+    if(file.size > IMAGE_UPLOAD_MAX_SIZE) {
+        toast.error(`Image size must be less than ${IMAGE_UPLOAD_MAX_SIZE / 1000}kb`)
+
+    fileInput.value = "";
+    return;
+    }
+
+    const formData = new FormData(); // if image size is ok, append it to the form so it can be sent
+    formData.append("file", file);
+
+    //TODO file upload
+    console.log(formData.get('file'))
+
+    fileInput.value = "";
+  }
+
+
 
   return (
     <div className="flex flex-col items-start gap-2">
       <Button type="button" variant="default"
-        onClick={handleImageUpload}>
+        onClick={handleChooseImage}>
         <ImageUpIcon />
         Upload Image
       </Button>
@@ -27,6 +53,7 @@ export default function ImageUploader() {
         name="file"
         type="file"
         accept="image/*"
+        onChange={handleChange}
       />
     </div>
   );
