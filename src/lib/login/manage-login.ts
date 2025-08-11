5d0568407c12cf6527856a1c052d942fcda56de9
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-08-11 17:53:04 -0300

    feat: signing jwt using jose lib

--- src/lib/login/manage-login.ts
+++ src/lib/login/manage-login.ts
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import { cookies } from "next/headers";
+import { SignJWT } from "jose";
 
 const BASE_64 = "base64";
 const UTF_8 = "utf-8";
@@ -10,6 +11,11 @@ const jwtEncodedKey = new TextEncoder().encode(jwtSecretKey);
 const loginExpSeconds = Number(process.env.LOGIN_EXPIRATION_SECONDS) || 3600;
 const loginExpStr = process.env.LOGIN_EXPIRATION_STRING || "1h";
 const loginCookieName = process.env.LOGIN_COOKIE_NAME || "loginSession";
+
+type JwtPayload = {
+  username: string;
+  expiresAt: Date;
+};
 
 export async function hashPassword(password: string) {
   const hash = await bcrypt.hash(password, 10);
@@ -24,7 +30,7 @@ export async function verifyPassword(password: string, base64hash: string) {
 
 export async function createLoginSession(username: string) {
   const expiresAt = new Date(Date.now() + loginExpSeconds * 1000);
-  const loginSession = username + "-login-session"; //JWT
+  const loginSession = await signJwt({ username, expiresAt });
   const cookieStore = await cookies();
 
   cookieStore.set(loginCookieName, loginSession, {
@@ -33,6 +39,17 @@ export async function createLoginSession(username: string) {
     sameSite: "strict", // “Only send this cookie when the request comes from the same site that set it, never send it with cross-site requests, not even when following a link.”
     expires: expiresAt,
   });
+}
+
+export async function signJwt(jwtPayload: JwtPayload) {
+  return new SignJWT(jwtPayload)
+    .setProtectedHeader({
+      alg: "HS256",
+      typ: "JWT",
+    })
+    .setIssuedAt()
+    .setExpirationTime(loginExpStr)
+    .sign(jwtEncodedKey);
 }
 
 export async function deleteLoginSession() {
